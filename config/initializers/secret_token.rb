946490514d995fba990f60892a9b4e3d3b61ffa9
Michael Ebert <michael@goconquer.com>
2013-01-08 00:06:16 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Events::Application.config.secret_token = '7ef1e51270829494e722401e01cfb8ed49571e07e2e83c6f32831360cea78d759481ba7f9e2b1f68c89b4b127e76af61442b2e58c337be7cc75d39fbd4988ead'
